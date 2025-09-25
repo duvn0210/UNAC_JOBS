e65b1e3ffbe93d47a4a112214c9e7358c085c178
duvan-virtualsoft <holger.esparza@virtualsoft.tech>
2025-09-25 11:50:09 -0500

    Proyecto inicial con React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        ¡Hola, Tailwind v4 con Vite 🚀!
+      </h1>
+    </div>
+  )
+}
+
+export default App
