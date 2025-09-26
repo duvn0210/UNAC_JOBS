4fff547d6c871580e1c3d2b4e4f04c5088d9a76f
duvn0210 <esparzaduvan3@gmail.com>
2025-09-26 13:18:02 -0500

    cambio color hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -52,7 +52,7 @@ export default function Header({ isLoggedIn }) {
                                     className="btn relative inline-flex items-center justify-start overflow-hidden font-medium transition-all rounded bg-[#e39e2d] group font-semibold text-lg px-6 py-2 rounded-xl"
                                     onClick={() => navigate("/register")}
                                 >
-                                    <span class="w-56 h-48 rounded bg-black absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                    <span class="w-56 h-48 rounded bg-[#A0804D] absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
                                     <span class="relative w-full text-left transition-colors duration-300 ease-in-out">Registrarse</span>
                                 </button>
                             </>
